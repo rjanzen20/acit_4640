5d8cdceb8b1bde7beef75c7e194377a8c3a0b737
robertjanzen <robertjanzenbc@gmail.com>
2019-09-16 12:41:47 -0700

    Create vm setup and network setup scripts

--- module02/host_network_setup.sh
+++ module02/host_network_setup.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 vboxmanage () { VBoxManage.exe "$@"; }
 
-vboxmanage natnetwork --netname net_4640 --network "192.168.250.0/24" --dhcp off --enable
-vboxmanage natnetwork modify --netname net_4640 --port-forward-4 "SSH ssh:tcp:[]:50022:[192.168.250.10]:22"
-vboxmanage natnetwork modify --netname net_4640 --port-forward-4 "HTTP http:tcp:[]:50022:[192.168.250.10]:80"
-vboxmanage natnetwork modify --netname net_4640 --port-forward-4 "HTTPS https:tcp:[]:50022:[192.168.250.10]:443"
-
-vboxmanage createvm --name "VM_ACIT4640" --basefolder "" --ostype RedHat_64 --register
-vboxmanage storagectl --add
-vboxmanage storageattach vm
-vboxmanage createmedium disk
-vboxmanage modifyvm -cpu -memory -boot
+vboxmanage natnetwork add --netname "net_4640" --network "192.168.250.0/24" --dhcp off --enable \
+    --port-forward-4 "ssh:tcp:[]:50022:[192.168.250.10]:22" \
+    --port-forward-4 "http:tcp:[]:50080:[192.168.250.10]:80" \
+    --port-forward-4 "https:tcp:[]:50443:[192.168.250.10]:443"
---
+++ module02/vm_setup.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+vboxmanage () { VBoxManage.exe "$@"; }
+
+VDI_PATH="C:\\Users\\rober\\VirtualBox VMs\\VM_ACIT4640\\VM_ACIT4640.vdi"
+ISO_PATH="C:\\Users\\rober\\VirtualBox VMs\\CentOS-7-x86_64-Minimal-1810.iso"
+BASE_FOLDER="C:\\Users\\rober\\VirtualBox VMs\\"
+VM_NAME="VM_ACIT4640"
+NAT_NET_NAME="net_4640"
+
+setup_system () {
+    vboxmanage createmedium disk \
+        --filename "${VDI_PATH}" \
+        --size 10000 \
+        --format VDI
+    vboxmanage createvm \
+        --name "${VM_NAME}" \
+        --basefolder "${BASE_FOLDER}" \
+        --ostype RedHat_64 \
+        --register
+    vboxmanage storagectl "${VM_NAME}" \
+        --name "IDE" \
+        --add ide \
+        --controller PIIX4 \
+        --portcount 2 \
+        --bootable on
+    vboxmanage storageattach "${VM_NAME}" \
+        --storagectl "IDE" \
+        --type dvddrive \
+        --port 1 \
+        --device 1 \
+        --medium "${ISO_PATH}"
+    vboxmanage storagectl "${VM_NAME}" \
+        --name "SATA" \
+        --add sata \
+        --controller IntelAhci \
+        --portcount 1 \
+        --bootable on
+    vboxmanage storageattach "${VM_NAME}" \
+        --storagectl "SATA" \
+        --type hdd \
+        --port 0 \
+        --device 0 \
+        --medium "${VDI_PATH}"
+    vboxmanage modifyvm "${VM_NAME}" \
+        --cpus 1 \
+        --memory 1024 \
+        --boot1 disk \
+        --audio none \
+        --nic1 natnetwork \
+        --cableconnected1 on \
+        --nat-network1 "${NAT_NET_NAME}"  
+}
+
+setup_system
+echo "VM Setup Complete."
